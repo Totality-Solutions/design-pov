e82b1c088d0e816cb126a9d6a8e65f84bf575b7a
Khushboo Yadav <khushboo@totality.solutions>
2026-05-08 12:46:14 +0530

    build issue resolved

--- app/(marketing)/edition/brands/page.tsx
+++ app/(marketing)/edition/brands/page.tsx
@@ -1,6 +1,7 @@
 import ApplySection from "@/components/edition26/brands/ApplySection";
 import Brands from "@/components/edition26/brands/Brands";
 import BrandsHero from "@/components/edition26/brands/BrandsHero";
+import BuildPartner from "@/components/edition26/brands/BuildPartner";
 import Sponsors from "@/components/edition26/brands/Sponsors";
 import React from "react";
 
