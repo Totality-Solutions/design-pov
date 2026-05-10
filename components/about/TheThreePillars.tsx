8f5333598ca9d9542071e84f99dd71a8fb775579
DeveloperAtRest <amanmishra0209@gmail.com>
2026-05-10 16:55:31 +0530

    bugs fixed

--- components/about/TheThreePillars.tsx
+++ components/about/TheThreePillars.tsx
@@ -155,7 +155,7 @@ const TheThreePillars: React.FC = () => {
                  - shadow: adds depth to the stack
                  - z-index: ensures the last card is at bottom, or you can manually set it
               */
-              className="pillar-image-trigger w-full h-screen flex flex-col md:border-b border-[#DFDFDF] last:border-b-0 relative 
+              className="pillar-image-trigger w-full lg:h-screen flex pt-4 pb-8 lg:pt-0 lg:pb-0 flex-col md:border-b border-[#DFDFDF] last:border-b-0 relative 
                          sticky lg:top-0 top-20 lg:relative bg-white"
               style={{ zIndex: index + 1 }}
             >
