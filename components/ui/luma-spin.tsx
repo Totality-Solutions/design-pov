76a45eb8f6e4f5533d9ed8ac37fd848cbe429625
khushboo-totality <khushboo@totality.solutions>
2026-08-12 17:15:07 +0530

    change spinner color white

--- components/ui/luma-spin.tsx
+++ components/ui/luma-spin.tsx
@@ -3,8 +3,8 @@
 export const Component = () => {
   return (
     <div className="relative w-[65px] aspect-square">
-      <span className="absolute rounded-[50px] animate-loaderAnim shadow-[inset_0_0_0_3px] shadow-gray-800 dark:shadow-gray-100" />
-      <span className="absolute rounded-[50px] animate-loaderAnim animation-delay shadow-[inset_0_0_0_3px] shadow-gray-800 dark:shadow-gray-100" />
+      <span className="absolute rounded-[50px] animate-loaderAnim shadow-[inset_0_0_0_3px] shadow-white" />
+      <span className="absolute rounded-[50px] animate-loaderAnim animation-delay shadow-[inset_0_0_0_3px] shadow-white" />
       <style jsx>{`
         @keyframes loaderAnim {
           0% {
